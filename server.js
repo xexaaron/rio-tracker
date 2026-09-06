6b5b266a8a0a19d9a4497be7280b701373f9ce4d
xexaaron <xexaaron@gmail.com>
2026-09-06 15:07:29 -0700

    add server

--- server.js
+++ server.js
@@ -137,6 +137,10 @@ app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
 
+const server = app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 server.on("close", () => {
     console.log("SERVER CLOSED");
 });
